02ba2c743e4a05d55534a9d05c1c1d56b814d72b
Katie <costantinkp@gmail.com>
2022-01-19 18:56:04 -0500

    initial commit- playing around with flex style of navbar/homepage

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { flexDirection: "column"}]}>
+      <View style={[styles.container, { flexDirection: "row"}]}>
+        <View style={{ flex: 1, backgroundColor: "#f3969a" }}/>
+        <View style={{ flex: 1, backgroundColor: "#ffce67" }}/>
+      </View>
+      <View style={[styles.container, { flexDirection: "row"}]}>
+        <View style={{ flex: 1, backgroundColor: "#ff7851" }}/>
+        <View style={{ flex: 1, backgroundColor: "#78c2ad" }}/>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
+
+export default App;
+
